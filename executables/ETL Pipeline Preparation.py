92f848632a5af21394ef58d9525db1cb40651b4c
elifinspace <elif.surmeli1@vodafone.com>
2019-09-28 20:07:51 +0100

    Correct codestyle problems,removes redundant code

--- executables/ETL Pipeline Preparation.py
+++ executables/ETL Pipeline Preparation.py
@@ -2,24 +2,14 @@
 # coding: utf-8
 
 # # ETL Pipeline Preparation
-# Follow the instructions below to help you create your ETL pipeline.
-# ### 1. Import libraries and load datasets.
-# - Import Python libraries
-# - Load `messages.csv` into a dataframe and inspect the first few lines.
-# - Load `categories.csv` into a dataframe and inspect the first few lines.
 
-# In[1]:
-
-
-# import necessary packages
+#  import necessary packages
 import pandas as pd
+import sys
 from sqlalchemy import create_engine
 
 
-# In[2]:
-
-
-def read_data(msg_file='messages.csv', cat_file='categories.csv'):
+def read_data(msg_file, cat_file):
     """
     Reads file paths of the two datasets to dataframes,merges on id
     Returns merged Pandas DataFrame
@@ -29,65 +19,59 @@ def read_data(msg_file='messages.csv', cat_file='categories.csv'):
     """
     messages = pd.read_csv(msg_file)
     categories = pd.read_csv(cat_file)
-    # merge datasets
+    #  merge datasets
     df = messages.merge(categories, on='id')
-    return df, categories
+    print('Messages and Categories DataFrames are read and merged.')
+    return df
 
 
-# In[3]:
-
-
-def process_data(df, categories):
+def process_data(df):
     """
-    Creates separate columns for 36 categories from categories DataFrame 
+    Creates separate columns for 36 categories
     Concat back to resultant DataFrame
     :param df: Messages and Categories DataFrame
-    :param categories: Categories DataFrame
-    :return df with categories encoded 
+    :return df with categories encoded
     """
     # create a dataframe of the 36 individual category columns
     categories = df.categories.str.split(';', expand=True)
     # select the first row of the categories dataframe
-    row = categories.loc[0,:]
+    row = categories.loc[0, :]
 
     # extracts a list of new column names for categories.
     category_colnames = row.str.slice(stop=-2).tolist()
     categories.columns = category_colnames
-    
+
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].str.slice(start=-1)
         # convert column from string to numeric
         categories[column] = categories[column].astype('int')
-    
-    # drop original column from merged DataFrame
+
+    #  drop original column from merged DataFrame
     df.drop('categories', axis=1, inplace=True)
-    # add splitted and renamed categories to final DataFrame
+    #  add splitted and renamed categories to final DataFrame
     df = pd.concat([df, categories], axis=1)
     # check duplicates
+    print("Record Count : {}".format(df.shape[0]))
     print("{} duplicates found to be removed".format(df.duplicated().sum()))
     # drop duplicates
     df.drop_duplicates(inplace=True)
+    print("Final Record Count : {}".format(df.shape[0]))
     return df
 
 
-# In[4]:
-
-
-def save_table(df, dbname, tablename):
+def save_table(df, dbname, tablename='messages'):
     """
-    Saves pandas DataFrame as specified tablename in the specified database 
+    Saves pandas DataFrame as specified tablename in the specified database
     :param df: Messages and Categories DataFrame
     :param dbname: sql database name
     :param tablename: sql table name
-    :return 
-    """   
+    :return
+    """
     engine = create_engine('sqlite:///{}'.format(dbname))
-    engine.execute('DROP TABLE IF EXISTS {}'.format(tablename)) # drop if exists
+    engine.execute('DROP TABLE IF EXISTS {}'.format(tablename))
     df.to_sql(tablename, engine, index=False)
-
-
-# In[5]:
+    print("{} Table is saved to : {}".format(tablename, dbname))
 
 
 def read_table(dbname, tablename):
@@ -96,22 +80,23 @@ def read_table(dbname, tablename):
     :param dbname: sql database name
     :param tablename: sql table name
     :return df: pandas DataFrame
-    """       
+    """
     engine = create_engine('sqlite:///{}'.format(dbname))
-    df =pd.read_sql_table(tablename, con=engine)
+    df = pd.read_sql_table(tablename, con=engine)
     return df
 
 
-# In[6]:
+if __name__ == '__main__':
+    """
+    Main functionality
+    """
+    # Example Execution:
+    # python process_data.py messages.csv categories.csv DisasterResponse.db
 
+    msg_file_path = sys.argv[1]
+    cat_file_path = sys.argv[2]
+    database_path = sys.argv[3]
 
-df, categories = read_data()
-df = process_data(df, categories)
-save_table(df, 'disaster.db', 'messages')
-
-
-# In[7]:
-
-
-# read_table('disaster.db', 'messages')
-
+    df = read_data(msg_file=msg_file_path, cat_file=cat_file_path)
+    df = process_data(df)
+    save_table(df, dbname=database_path)
